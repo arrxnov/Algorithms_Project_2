462612a0fe6abf4b6f2210240d4e2523e66fe799
arrxnov <jcbgrd@gmail.com>
2024-02-27 13:38:38 -0500

    Skeleton Code

--- Algorithms_Project_2/main.cpp
+++ Algorithms_Project_2/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+//
+//	For all the solution functions, return an int array with
+//  [ <len of solution>, solution[0], solution[1] ... solution[n] ]
+//
+
+int* bottomUp(int problem, int* denoms, int numDenoms, int* subps = NULL)
+{
+
+}
+
+int* recursive(int problem, int* denoms, int numDenoms, int* subps = NULL)
+{
+
+}
+
+int* memoize(int problem, int* denoms, int numDenoms, int* subps = NULL)
+{
+
+}
+
 int main()
 {
 	using namespace std;
@@ -13,7 +33,14 @@ int main()
 	int* problems = new int[numProblems];
 	for (int i = 0; i < numProblems; i++) cin >> problems[i];
 
+	for (int i = 0; i < numProblems; i++)
+	{
+		int* solution = bottomUp(problems[i], denoms, numDenoms);
+		
+		int* solution = memoize(problems[i], denoms, numDenoms);
 
+		int* solution = recursive(problems[i], denoms, numDenoms);
+	}
 
 
 	return 0;
